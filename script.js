4208e136a5c90625d673e5a3916581cec329b272
Glizzy-Tulaxi17 <bebeandyto@gmail.com>
2025-09-10 18:00:32 -0300

    Agrege Archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+function logearse(){
+    var username = document.getElementById('txtUsuario').value;
+    var password = document.getElementById('txtPassword').value;
+    alert('Bienvenido: '+username+' Tu Contraseña Es: '+password);
+
+
+}
